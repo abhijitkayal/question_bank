dbf6f6dbe3d1302ba3d4dc2243492e4be2796601
abhijitkayal <153314653+abhijitkayal@users.noreply.github.com>
2025-08-15 09:43:11 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Practice from "@/components/practice";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <Practice/>
+    </>
+   
+  );
+}
